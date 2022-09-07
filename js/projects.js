72f7c9cfd56e404ddcf0c974b8a67c0e10b9b87f
Micheal <oguntayomicheal@gmail.com>
2022-09-07 18:58:00 +0100

    Update projects.js file to create project section dynamically

--- js/projects.js
+++ js/projects.js
@@ -1,15 +1,82 @@
-
-
 const projectData = [
     {
-        name : "Keeping track of hundreds of components",
+        name : "Multi-Post Stories Gain+Glory",
         description : "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
         featuredImage : "./images/Project-images/Snapshoot_Portfolio.png",
         technologies : {lang1 : "ruby", lang2 : "html", lang3 : "css",lang4 :  "javascript"},
         liveLink : " https://mike-devloper.github.io/Portfolio-Mobile/",
         sourceLink : "https://github.com/Mike-devloper/Portfolio-Mobile",
     },
+    {
+      name : "Multi-Post Stories Gain+Glory",
+      description : "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
+      featuredImage : "./images/Project-images/Snapshoot_Portfolio.png",
+      technologies : {lang1 : "ruby", lang2 : "html", lang3 : "css",lang4 :  "javascript"},
+      liveLink : " https://mike-devloper.github.io/Portfolio-Mobile/",
+      sourceLink : "https://github.com/Mike-devloper/Portfolio-Mobile",
+  },
+  {
+    name : "Multi-Post Stories Gain+Glory",
+    description : "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
+    featuredImage : "./images/Project-images/Snapshoot_Portfolio.png",
+    technologies : {lang1 : "ruby", lang2 : "html", lang3 : "css",lang4 :  "javascript"},
+    liveLink : " https://mike-devloper.github.io/Portfolio-Mobile/",
+    sourceLink : "https://github.com/Mike-devloper/Portfolio-Mobile",
+},
+{
+  name : "Multi-Post Stories Gain+Glory",
+  description : "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
+  featuredImage : "./images/Project-images/Snapshoot_Portfolio.png",
+  technologies : {lang1 : "ruby", lang2 : "html", lang3 : "css",lang4 :  "javascript"},
+  liveLink : " https://mike-devloper.github.io/Portfolio-Mobile/",
+  sourceLink : "https://github.com/Mike-devloper/Portfolio-Mobile",
+},
+{
+  name : "Multi-Post Stories Gain+Glory",
+  description : "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
+  featuredImage : "./images/Project-images/Snapshoot_Portfolio.png",
+  technologies : {lang1 : "ruby", lang2 : "html", lang3 : "css",lang4 :  "javascript"},
+  liveLink : " https://mike-devloper.github.io/Portfolio-Mobile/",
+  sourceLink : "https://github.com/Mike-devloper/Portfolio-Mobile",
+},
+{
+  name : "Multi-Post Stories Gain+Glory",
+  description : "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
+  featuredImage : "./images/Project-images/Snapshoot_Portfolio.png",
+  technologies : {lang1 : "ruby", lang2 : "html", lang3 : "css",lang4 :  "javascript"},
+  liveLink : " https://mike-devloper.github.io/Portfolio-Mobile/",
+  sourceLink : "https://github.com/Mike-devloper/Portfolio-Mobile",
+},
 ]
+
+  document.querySelector('.project_large_screen').innerHTML = projectData.map(
+      (info) => `
+      <div class="grid-container">
+          <div class="grid-item1"></div>
+              <div class="grid-item">
+                  <div class="center1">
+                      <h3 class="work_title">${info.name}</h3>
+                      <div class="boxed_flex">
+                          <div class="boxed">${info.technologies.lang1}</div>
+                          <div class="boxed">${info.technologies.lang2}</div>
+                          <div class="boxed">${info.technologies.lang3}</div>
+                          <div class="boxed">${info.technologies.lang4}</div>
+                      </div>
+                    <form action="work.html">
+                          <button type="button" class="button2 See_Project btn1">See Project</button>
+                    </form> 
+                </div>
+          </div>
+      </div>
+ `,
+ )
+.join('');
+
+for (let i = 0; i <= projectData.length; i += 1) {
+  if (i % 2 === 1) {
+    document.querySelectorAll('.grid-container')[i].classList.add('order');
+  }
+}     
 
 
 document.querySelector('.project').innerHTML = projectData.map(
@@ -33,13 +100,13 @@ document.querySelector('.project').innerHTML = projectData.map(
           </div>
 
           <div class="desk_pop_btn">
-            <div class="poplinks">
-            <a href="${info.liveLink}" class="pop_button">
+            <div class="desk_poplinks">
+            <a href="${info.liveLink}" class="desk_pop_button">
               See Live
               <img src="./icons/popup/live_Icon.png" alt="live site icon" class="pop_icon"/>
             </a>
                <br>
-            <a href="${info.sourceLink}" class="pop_button">
+            <a href="${info.sourceLink}" class="desk_pop_button">
                See Source
                 <img src="./icons/popup/Icon_GitHub.png" alt="github icon" class="pop_icon"/></a>
              </div>
@@ -77,6 +144,7 @@ document.querySelectorAll('.See_Project').forEach((See_Project) => {
       });
     });
   });
+
 
 
 document.querySelectorAll('.close').forEach((close) => {
